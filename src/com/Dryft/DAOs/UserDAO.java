ce188c07f15791e6189e91cc8a099d754f7ac061
Kushagra Gupta <39802979+Kushagra-0801@users.noreply.github.com>
2019-11-11 02:15:55 +0530

    fix various errors (#23)
    optimize imports
    declare array java-style
    fix `st.setString()` call in `createUser()`
    fix unknown reference to a user object
    use `.equals()` method for string comparison
    some other things

--- src/com/Dryft/DAOs/UserDAO.java
+++ src/com/Dryft/DAOs/UserDAO.java
@@ -1,22 +1,30 @@
 package com.Dryft.DAOs;
 
-import java.sql.*;
+import com.Dryft.exceptions.UserSideException;
 import com.Dryft.models.User;
 import com.Dryft.utils.DBConn;
 import com.Dryft.utils.Hasher;
-import com.Dryft.exceptions.UserSideException;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class UserDAO {
 
     public static void createUser(User user) throws SQLException {
         Connection conn = DBConn.getConn();
         checkUserAlreadyExists(user, conn);
-        String passCredentials[] = Hasher.hashPassword(user.getPassword());
+        String[] passCredentials = Hasher.hashPassword(user.getPassword());
         String query = "Insert into users values (?,?,?,?,?,?)";
         PreparedStatement st = conn.prepareStatement(query);
-        st.setString(6, user.getEmail(), user.getFullname(), passCredentials[0], passCredentials[1], user.getSex(),
-                user.getBalance());
-        ResultSet result = st.executeQuery();
+        st.setString(1, user.getEmail());
+        st.setString(2, user.getFullname());
+        st.setString(3, passCredentials[0]);
+        st.setString(4, passCredentials[1]);
+        st.setString(5, String.valueOf(user.getSex()));
+        st.setInt(6, user.getBalance());
+        st.executeQuery();
         DBConn.closeConn();
     }
 
@@ -30,29 +38,28 @@ public class UserDAO {
     }
 
     private static void validateCredentials(String email, String password, Connection conn) throws SQLException {
-        String query = "Select password,salt from users where email = (?)";
+        String query = "Select password, salt from users where email = (?)";
         PreparedStatement st = conn.prepareStatement(query);
-        st.setString(1, user.getEmail());
+        st.setString(1, email);
         ResultSet result = st.executeQuery();
         if (result.next()) {
             String hashedPassword = Hasher.hashPasswordWithSalt(password, result.getString("salt"));
-            if (!(result.getString("email") == email && result.getString("password") == hashedPassword))
+            if (!(result.getString("email").equals(email) && result.getString("password").equals(hashedPassword)))
                 throw new UserSideException(UserSideException.ErrorCode.InvalidCredentials);
         } else
             throw new UserSideException(UserSideException.ErrorCode.UserNotFound);
-
     }
 
     public static User retrieveUserDetails(String email, String password) throws SQLException {
         Connection conn = DBConn.getConn();
         validateCredentials(email, password, conn);
-        String query = "Select fullname,sex,balance from users where email = (?)";
+        String query = "Select fullname, sex, balance from users where email = (?)";
         PreparedStatement st = conn.prepareStatement(query);
-        st.setString(1, user.getEmail());
+        st.setString(1, email);
         ResultSet result = st.executeQuery();
         result.next();
         String fullname = result.getString("fullname");
-        char sex = result.getCharacterStream("sex");
+        char sex = result.getString("sex").charAt(0);
         int balance = result.getInt("balance");
         User user = new User(fullname, email, password, sex, balance);
         DBConn.closeConn();
