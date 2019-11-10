f8bd289ecfd8fc79be9c11d2e363a65e4f2fb267
Aviral Agarwal <aviralagarwal25@gmail.com>
2019-11-11 00:51:30 +0530

    Add tables for drivers,cars,locations and rides

--- src/tools/PopulateDB.java
+++ src/tools/PopulateDB.java
@@ -8,6 +8,10 @@ import java.sql.Statement;
 public class PopulateDB {
     private static void makeTables(Connection conn) {
         makeUserTable(conn);
+        makeDriverTable(conn);
+        makeCarTable(conn);
+        makeLocationTable(conn);
+        makeRideTable(conn);
     }
 
     private static void makeUserTable(Connection conn) {
@@ -23,6 +27,81 @@ public class PopulateDB {
         try {
             stmt = conn.createStatement();
             stmt.execute(createUserTable);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void makeDriverTable(Connection conn) {
+        String createDriverTable = "CREATE TABLE drivers (" +
+                "id INT PRIMARY KEY," +
+                "name TEXT," +
+                "carNumber INT," +
+                "sex CHAR(1)," +
+                "location TEXT," +
+                "rating REAL," +
+                "reviews TEXT," +
+                "FOREIGN KEY(carNumber) REFERENCES cars(licenseNumber)," +                
+                ");";
+        Statement stmt = null;
+        try {
+            stmt = conn.createStatement();
+            stmt.execute(createDriverTable);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void makeCarTable(Connection conn) {
+        String createCarTable = "CREATE TABLE cars (" +
+                "licenseNumber TEXT PRIMARY KEY," +
+                "model TEXT," +
+                "costPerKm TEXT," +
+                "CarType TEXT," +
+                "speed INT" +
+                ");";
+        Statement stmt = null;
+        try {
+            stmt = conn.createStatement();
+            stmt.execute(createCarTable);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void makeLocationTable(Connection conn) {
+        String createLocationTable = "CREATE TABLE locations (" +
+                "name TEXT PRIMARY KEY," +
+                "x INT," +
+                "y INT," +
+                ");";
+        Statement stmt = null;
+        try {
+            stmt = conn.createStatement();
+            stmt.execute(createLocationTable);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void makeRideTable(Connection conn) {
+        String createRideTable = "CREATE TABLE rides (" +
+                "id INT PRIMARY KEY," +
+                "userEmail TEXT ," +
+                "driver TEXT," +
+                "source TEXT," +
+                "destination TEXT," +
+                "car TEXT," +
+                "startTime TEXT," +
+                "duration INT," +
+                "distance INT," +
+                "cost INT," +
+                "FOREIGN KEY(userEmail) REFERENCES users(email)," +
+                ");";
+        Statement stmt = null;
+        try {
+            stmt = conn.createStatement();
+            stmt.execute(createRideTable);
         } catch (SQLException e) {
             e.printStackTrace();
         }
