de898425425b9c9425942a894fd952eeee97bd14
Aviral Agarwal <aviralagarwal25@gmail.com>
2019-11-11 00:31:04 +0530

    Add Ride model
    This model stores all ride related data such as startTime, Driver, start and end locations etc

---
+++ src/com/Dryft/models/Ride.java
@@ -0,0 +1,74 @@
+package com.Dryft.models;
+
+import java.lang.Math;
+import java.time.LocalDateTime;
+
+class Ride {
+    private final Location source;
+    private final Location destination;
+    private final Driver driver;
+    private final User user;
+    private final LocalDateTime startTime;
+    private final int distance;
+    private final int duration;
+    private final int cost;
+
+    Ride(Location source, Location destination, Driver driver, User user) {
+        int distance = calculateDistance(source, destination);
+        int duration = calculateDuration(distance, driver);
+        int cost = calculateCost(distance, driver);
+        this.source = source;
+        this.destination = destination;
+        this.driver = driver;
+        this.user = user;
+        this.startTime = LocalDateTime.now();
+        this.distance = distance;
+        this.duration = duration;
+        this.cost = cost;
+    }
+
+    // Calculates the Manhattan Distance
+    public static int calculateDistance(Location source, Location destination) {
+        return Math.abs(destination.getX() - source.getX()) + Math.abs(destination.getY() - source.getY());
+    }
+
+    public static int calculateDuration(int distance, Driver driver) {
+        return distance / driver.getCar().getSpeed();
+    }
+
+    public static int calculateCost(int distance, Driver driver) {
+        return distance * driver.getCar().getCostPerKm();
+    }
+
+    public Location getSource() {
+        return source;
+    }
+
+    public Location getDestination() {
+        return destination;
+    }
+
+    public Driver getDriver() {
+        return driver;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public int getStartTime() {
+        return startTime;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+}
